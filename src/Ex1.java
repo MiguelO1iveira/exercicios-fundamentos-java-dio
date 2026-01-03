b784974da84e65b8bfa998c4a415f558ee1068a0
Miguel José Rodrigues de Oliveira <miguel.roliveira.jose@gmail.com>
2026-01-03 09:55:09 -0300

    Exercício 1 Finalizado

---
+++ src/Ex1.java
@@ -0,0 +1,32 @@
+import java.time.OffsetDateTime;
+import java.util.Scanner;
+
+public class Ex1 {
+
+	public static void main(String[] args) {
+		try (Scanner scanner = new Scanner(System.in)) {
+			// Variáveis
+			int baseYear = OffsetDateTime.now().getYear();
+			String name;
+			int year, age;
+			
+			System.out.println("Informe o seu nome: ");
+			System.out.print(">>> ");
+			name = scanner.nextLine();
+			
+			System.out.println("Informe o seu ano de nascimento: ");
+			System.out.print(">>> ");
+			year = Integer.parseInt(scanner.nextLine());
+			
+			age = baseYear - year;
+			
+			System.out.printf("Olá, %s, você tem %s anos!\n", name, age);
+			
+			
+			
+			
+		} // Fecha o try
+
+	}
+
+}
